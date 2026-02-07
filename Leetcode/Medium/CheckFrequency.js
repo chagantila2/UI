2e62a1aad8780ff2df54dadd7e11db013082e19a
chagantila2 <lakshmich2702@gmail.com>
2026-02-07 17:03:23 -0600

    check frequesncy

---
+++ Leetcode/Medium/CheckFrequency.js
@@ -0,0 +1,30 @@
+
+/**
+ * Write a program to count the frequency of each
+ *  element in an array.
+ */
+
+
+var arr = [10, 4, 7, 8, 3, 7, 4, 3]
+
+var visited = []
+
+for (var i = 0; i < arr.length; i++) {
+
+    if (visited[i]) {
+        continue
+    }
+
+    var counter = 0;
+
+    for (var j = 0; j < arr.length; j++) {
+
+        if (arr[i] == arr[j]) {
+            counter++;
+            visited = true
+        }
+    }
+    console.log(arr[i] + " :" + counter)
+}
+
+
